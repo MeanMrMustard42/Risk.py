53090657dbbf87a1c741844e73f2ba09b3262316
MeanMrMustard42 <stevensuarez426@gmail.com>
2021-09-03 23:42:33 -0700

    Renamed Risk.py to gameplay.py because VSCode wasn't having any of it

---
+++ gameplay.py
@@ -0,0 +1,80 @@
+#from PIL import Image
+from DiceRoller import DiceRoller
+from Player import Player
+from Territory import Territory
+
+
+allTerritories = []
+adjacentCountries = []
+numTerritories = 0
+gameIsRunning = False
+print("Hello world")
+#f = open("game.txt", "w")
+
+dice = DiceRoller()
+p1 = Player()
+p2 = Player()
+p3 = Player()
+p4 = Player()
+
+players = [p1, p2, p3, p4] # Might be interesting to support a dynamic amount of players later
+ 
+#board = Image.open(r"C:\Users\steve\Desktop\risk.jpg")
+with open('riskdata.txt') as f:
+    data = f.readlines()
+
+#board.show()
+
+
+#Each country will start out with a random army count of 1-6
+def playerOnePick():
+    global adjacentCountries
+    country = Territory(data[0], dice.getRoll("1d6"), adjacentCountries, p1) 
+
+def playerTwoPick():
+    global adjacentCountries
+    country = Territory(data[0], dice.getRoll("1d6"), adjacentCountries, p2)
+
+def playerThreePick():
+    global adjacentCountries
+    country = Territory(data[0], dice.getRoll("1d6"), adjacentCountries, p3)
+
+def playerFourPick():
+    global adjacentCountries
+    country = Territory(data[0], dice.getRoll("1d6"), adjacentCountries, p4)
+
+def pickTerritories(roll):
+    global numTerritories
+    global data
+    global allTerritories
+
+    for line in data:
+        temp = data[line].split("-")     
+        numTerritories += 1
+        adjacentCountries = data[line].split(", ") #TODO: See if the brackets [] in riskdata.txt cause any problems with loading in these names
+
+        switcher = {
+            1: playerOnePick,
+            2: playerTwoPick,
+            3: playerThreePick,
+            4: playerFourPick
+        }
+        switcher.get(roll, "Only four players allowed right now")
+
+
+def play():
+    pickTerritories()
+    gameIsRunning = True   
+
+
+    while(gameIsRunning):
+        for player in players:
+            player.reinforce()
+            player.attack()
+            player.fortify()
+            if(player.hasWon):
+                print("Player " + player.getName() + " has won!")
+                gameIsRunning = False
+        
+def main():
+    print("what the fUCK is this running")
